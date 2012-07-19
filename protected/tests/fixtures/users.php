2bc5d6d9f6990608db34b78844c825a7e0c604c9
Charles Gultom <charles@w3magz.com>
2012-07-19 23:38:18 +0700

    Add Users fixture file

---
+++ protected/tests/fixtures/users.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+    'user1' => array (
+        'email' => 'test1@notaddress.com',
+        'username' => 'Test_User_One',
+        'password' => md5('test1'),
+        'last_login_time' => null,
+        'create_time' => null,
+        'create_userid' => null,
+        'update_time' => null,
+        'update_userid' => null
+    ),
+    'user2' => array (
+        'email' => 'test1@notaddress.com',
+        'username' => 'Test_User_One',
+        'password' => md5('test2'),
+        'last_login_time' => null,
+        'create_time' => null,
+        'create_userid' => null,
+        'update_time' => null,
+        'update_userid' => null
+    )
+)
+?>
