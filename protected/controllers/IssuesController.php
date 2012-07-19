8ca149a898544104b916dec63fb0726034769c42
Charles Gultom <charles@w3magz.com>
2012-07-19 13:50:36 +0700

    Set project_id on actionCreate() method

--- protected/controllers/IssuesController.php
+++ protected/controllers/IssuesController.php
@@ -63,6 +63,7 @@ class IssuesController extends Controller
 	public function actionCreate()
 	{
 		$model=new Issues;
+                $model->project_id = $this->_project->id;
 
 		// Uncomment the following line if AJAX validation is needed
 		// $this->performAjaxValidation($model);
