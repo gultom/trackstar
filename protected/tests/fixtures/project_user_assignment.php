ad04612b9fb7ce60a9a3e4df2c4f038864cc17fc
Charles Gultom <charles@w3magz.com>
2012-07-19 23:37:47 +0700

    Add Project User Assignment fixture file

---
+++ protected/tests/fixtures/project_user_assignment.php
@@ -0,0 +1,21 @@
+<?php
+
+return array (
+    'user1ToProject1' => array (
+        'project_id' => 1,
+        'user_id' => 1,
+        'create_time' => null,
+        'create_userid' => null,
+        'update_time' => null,
+        'update_userid' => null
+    ),
+    'user2ToProject1' => array (
+        'project_id' => 1,
+        'user_id' => 2,
+        'create_time' => null,
+        'create_userid' => null,
+        'update_time' => null,
+        'update_userid' => null
+    )
+)
+?>
