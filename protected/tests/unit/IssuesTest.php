259111582f374fe2b64abda763d8301bd8a4d990
Charles Gultom <charles@w3magz.com>
2012-07-20 17:17:48 +0700

    Add fixture attribute, method testGetStatusText and testGetTypeText

--- protected/tests/unit/IssuesTest.php
+++ protected/tests/unit/IssuesTest.php
@@ -2,6 +2,11 @@
 
 class IssuesTest extends CDbTestCase {
     
+    public $fixtures = array (
+        'projects' => 'Projects',
+        'issues' => 'Issues'
+    );
+    
     public function testGetTypes() {
         $options = Issues::model()->typeOptions;
         $this->assertTrue(is_array($options));
@@ -9,6 +14,14 @@ class IssuesTest extends CDbTestCase {
         $this->assertTrue(in_array('Bug', $options));
         $this->assertTrue(in_array('Feature', $options));
         $this->assertTrue(in_array('Task', $options));
+    }
+    
+    public function testGetStatusText() {
+        $this->assertTrue('OPEN' == $this->issues('issueBug')->getStatusText());
+    }
+    
+    public function testGetTypeText() {
+        $this->assertTrue('Bug' == $this->issues('issueBug')->getTypeText());
     }
 }
 ?>
