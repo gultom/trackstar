6c10dea384046dcdf88206b940c47a2eb0956cdd
Charles Gultom <charles@w3magz.com>
2012-07-30 01:10:04 +0700

    Create new model, SubClass from CActiveRecord

---
+++ protected/models/TrackStarActiveRecord.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Description of TrackStarActiveRecord
+ *
+ * @author Charles
+ */
+abstract class TrackStarActiveRecord extends CActiveRecord {
+    
+    /**
+     * Prepare create_time, create_userid, update_time, update_userid
+     * attribute before performing validation 
+     */
+    protected function beforeValidate() {
+        if ($this->newRecord) {
+            // set the create date, last updated date and the user doing the creating
+            $this->create_time = $this->update_time = new CDbExpression('NOW()');
+            $this->create_userid = $this->update_userid = Yii::app()->user->id;
+        }
+        else {
+            $this->update_time = new CDbExpression('NOW()');
+            $this->update_userid = Yii::app()->user->id;
+        }
+        
+        return parent::beforeValidate();
+    }
+}
+
+?>
