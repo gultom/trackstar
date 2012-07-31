6167c62360244df23ba9010180909ac28e8d0ff4
Charles Gultom <charles@w3magz.com>
2012-07-31 11:37:05 +0700

    Change ->newRecord to ->isNewRecord @beforeValidate method

--- protected/models/TrackStarActiveRecord.php
+++ protected/models/TrackStarActiveRecord.php
@@ -12,7 +12,7 @@ abstract class TrackStarActiveRecord extends CActiveRecord {
      * attribute before performing validation 
      */
     protected function beforeValidate() {
-        if ($this->newRecord) {
+        if ($this->isNewRecord) {
             // set the create date, last updated date and the user doing the creating
             $this->create_time = $this->update_time = new CDbExpression('NOW()');
             $this->create_userid = $this->update_userid = Yii::app()->user->id;
