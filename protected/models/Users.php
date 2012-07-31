1339f51307edd0354f0a6f15a27c5005a1a3ef0d
Charles Gultom <charles@w3magz.com>
2012-07-31 23:47:47 +0700

    Added password repeat, and change some rules

--- protected/models/Users.php
+++ protected/models/Users.php
@@ -23,6 +23,8 @@
  */
 class Users extends TrackStarActiveRecord
 {
+    public $password_repeat;
+    
 	/**
 	 * Returns the static model of the specified AR class.
 	 * @param string $className active record class name.
@@ -49,9 +51,11 @@ class Users extends TrackStarActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-            array('email, username', 'required'),
+            array('email, username, password', 'required'),
 			array('email, username, password', 'length', 'max'=>256),
             array('username, email', 'unique'),
+            array('password', 'compare'),
+            array('password_repeat', 'safe'),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
 			array('id, email, username, password, last_login_time, create_time, create_userid, update_time, update_userid', 'safe', 'on'=>'search'),
